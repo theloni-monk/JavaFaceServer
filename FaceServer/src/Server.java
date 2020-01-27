a98320d168a523e69eeee3cdb5dfab73abc28357
Theo <theo.acooper@gmail.com>
2020-01-27 06:13:48 -0800

    added facial recognition

--- FaceServer/src/Server.java
+++ FaceServer/src/Server.java
@@ -3,10 +3,7 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.objdetect.Objdetect;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.BufferedInputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
@@ -35,27 +32,57 @@ class ServerThread implements Runnable {
     Socket sock;
     int[] imgDim;
     Mat frame;
-    //TODO: add classifier
+    BufferedInputStream inputStream;
+    DataOutputStream outputStream;
+    private CascadeClassifier faceCascade;
+    private int absoluteFaceSize;
+
     ServerThread(Socket s, int[] imgDim){
         this.sock = s;
         this.imgDim = imgDim;
         this.frame = new Mat(imgDim[0],imgDim[1], CvType.CV_8UC3);
+        this.absoluteFaceSize = 0;
+
+        //load face detector
+        //TODO: fix file path
+        String haar = "C:\\Users\\TheoA\\Documents\\Github\\JavaFaceServer\\FaceServer\\src\\haarcascade.xml";
+        String lbp =  "C:\\Users\\TheoA\\Documents\\Github\\JavaFaceServer\\FaceServer\\src\\lbp.xml";
+
+        this.faceCascade = new CascadeClassifier(lbp);
+
     }
     public void run() {
         try{
-            BufferedInputStream inputStream = new BufferedInputStream(sock.getInputStream());
-            BufferedOutputStream outputStream = new BufferedOutputStream(sock.getOutputStream());
+            //initialize I/O streams
+            inputStream = new BufferedInputStream(sock.getInputStream());
+            outputStream = new DataOutputStream(sock.getOutputStream());
+
             while(true) {
-
                 byte[] ready = new byte[1];
-                inputStream.read(ready); //checks for ready byte in stream
-
+                inputStream.read(ready); //checks for ready byte in stream to ingest the rest of the frame
+                //socket streams don't operate on a message system, so ready byte is necessary to signal message
                 if(ready[0] == 0x01) {
-                    byte[] imageAr = new byte[921600]; //TEMP 480p
+                    byte[] imageAr = new byte[921600]; //TEMP allocate 480p byte buffer to write to
                     inputStream.read(imageAr);
                     frame.put(0, 0, imageAr); //put image matrix data from stream in bytearray to local frame field
-                    System.out.println("Ingested frame");
-                    //TODO: detect faces in frame and send back string with location
+
+                    //System.out.println("ingested frame");
+
+                    Rect[] faces = findFaces();
+
+                    //hack gets around native array access issues
+                    boolean faceFound = true;
+                    try{Rect f = faces[0];}
+                    catch(ArrayIndexOutOfBoundsException e){ faceFound = false; }
+
+                    if(faceFound){
+                        System.out.println("detected face(s)");
+                        sendFaces(faces);
+                    }
+                    else{
+                        outputStream.write(0x00); // tell client no faces were found
+                    }
+                    //FIXME: hangs here
                 }
             }
         }
@@ -69,6 +96,51 @@ class ServerThread implements Runnable {
             } catch(IOException ioe) {
                 ioe.printStackTrace();
             }
+        }
     }
-}
+    private Rect[] findFaces(){
+        MatOfRect faces = new MatOfRect();
+        Mat grayFrame = new Mat();
+
+        // convert the frame in gray scale
+        Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
+        // equalize the frame histogram to improve the result
+        Imgproc.equalizeHist(grayFrame, grayFrame);
+
+        // compute minimum face size (10% of the frame height, in our case)
+        if (this.absoluteFaceSize == 0)
+        {
+            int height = grayFrame.rows();
+            if (Math.round(height * 0.1f) > 0)
+            {
+                this.absoluteFaceSize = Math.round(height * 0.2f);
+            }
+        }
+
+        // detect faces
+        //FIXME: fuck this shit
+        this.faceCascade.detectMultiScale(grayFrame, faces, 1.1, 1, Objdetect.CASCADE_SCALE_IMAGE,
+                new Size(this.absoluteFaceSize, this.absoluteFaceSize), new Size());
+
+        //return the list of faces in Rect[] form
+        Rect[] rects = faces.toArray();
+        return rects;
+    }
+
+    private void sendFaces(Rect[] faces){
+        try {
+            outputStream.write(0x01);
+            System.out.println("Faces.length: "+faces.length);
+            outputStream.writeInt(faces.length);
+            for (Rect face : faces) {
+                outputStream.writeInt(face.x);
+                outputStream.writeInt(face.y);
+                outputStream.writeInt(face.width);
+                outputStream.writeInt(face.height);
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
